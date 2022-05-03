fe23e49a1ea361317a04aaf23a4a9855e413f7b9
Josef Kopriva <jkopriva@redhat.com>
2022-05-03 09:44:13 +0200

    fix

--- hack/util-set-quality-dashboard-backend-route.sh
+++ hack/util-set-quality-dashboard-backend-route.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/..
-TEMPLATE_FILE="$ROOT/components/quality-dashboard/frontend/backend-url-patch-template.yaml"
 PATCH_FILE="$ROOT/components/quality-dashboard/frontend/backend-url-patch.yaml"
 export BACKEND_ROUTE=$1
+TMP_FILE=$(mktemp)
 
-envsubst < $TEMPLATE_FILE > $PATCH_FILE
+envsubst < $PATCH_FILE > $TMP_FILE
+mv "$TMP_FILE" "$PATCH_FILE"
